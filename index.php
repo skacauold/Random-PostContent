3ff7a0937feca57373bb411d4523a45458b8be1c
MUHAMAD FIKRI <78306575+skacauold@users.noreply.github.com>
2023-07-19 17:55:05 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Random Blog Post</title>
+</head>
+<body>
+  <h1 id="title">Loading...</h1>
+  <p id="content"></p>
+
+  <script src="script.js"></script>
+</body>
+</html>
